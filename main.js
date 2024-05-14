7de2beacf64fd667a86bd2c4471d8e40ecf45a1d
maria <mariafernanda@uchicago.edu>
2024-05-14 18:56:45 -0500

    first popup

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function openPopup () {
+ let html = 'https://www.youtube.com/embed/nQiS0UasJW4?si=nrdrBVF8uwY11FTm'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=500,top=0x'
+ window.open(html, w, options)
+}
