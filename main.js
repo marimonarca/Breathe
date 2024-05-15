46fe5b6afe5be238c3bba065ae0d4772844f6a3f
maria <mariafernanda@uchicago.edu>
2024-05-15 02:10:22 -0500

    hover target

--- main.js
+++ main.js
@@ -39,10 +39,75 @@ function openMultiplePopups() {
 }
 
 
-
-// Event listener for the scroll event
+// Event listener for the scroll event//
 window.addEventListener('scroll', function() {
   if (isScrollBottom()) {
     openMultiplePopups();
   }
+});
+
+
+// hover and blackout effect//
+document.addEventListener('DOMContentLoaded', (event) => {
+  const blackoutDiv = document.createElement('div');
+  blackoutDiv.classList.add('blackout');
+  document.body.appendChild(blackoutDiv);
+
+  // Function to close all popups//
+  function closePopups() {
+    window.open('https://www.youtube.com/embed/nQiS0UasJW4?si=nrdrBVF8uwY11FTm', 'mexicohmr').close();
+    window.open('https://www.youtube.com/watch?v=-euUGPQZoHw', 'spiderman').close();
+    window.open('https://www.tiktok.com/@pappyorion/video/7360692095633411361?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530', 'congo').close();
+    window.open('https://www.univision.com/delicioso/30-recetas-altas-en-proteinas-y-bajas-en-carbohidratos-que-amaras-conocer', 'recetas').close();
+    window.open('https://linktr.ee/Tdrfund.org', 'tigraylinks').close();
+    window.open('https://youtu.be/2p3zZoraK9g?si=6-Fljw_yORfWMEMU', 'sza').close();
+    window.open('https://youtu.be/vT0Y3k32QRE?si=-wsmldOVeycDCBPT', 'eeaao').close();
+    window.open('https://www.youtube.com/watch?v=GWriwq7scRA', 'sudanhrw').close();
+    window.open('https://chicagomaroon.com/25853/news/rallies-sit-ins-padlocks-history-trauma-activism/', 'maroon').close();
+    window.open('https://positivepsychology.com/cbt-cognitive-behavioral-therapy-techniques-worksheets/', 'cbt').close();
+    window.open('https://m.media-amazon.com/images/I/918ckvZTCsL._AC_UF1000,1000_QL80_.jpg', 'cobaltred').close();
+    window.open('https://www.hrw.org/modal/34478', 'sudanpic').close();
+    window.open('https://www.hrw.org/news/2023/06/01/ethiopia-ethnic-cleansing-persists-under-tigray-truce', 'tigray').close();
+    window.open('https://www.theguardian.com/global/2018/oct/04/ontario-six-nations-nestle-running-water', 'nestle').close();
+    window.open('https://sapa-usa.org/Hope-for-Sudan/?fbclid=PAAaZMr4VAjKinWXZXucTTx82fQxYafFbbIHnVsdPVT99U6I3vXyohAJvi-Z4_aem_AURgfG7WDmkUyTlraghddZvjJ5hZm0dwAlvMNLoHM5s8xB7H4RjwC9huUkRpF0ZTG0I', 'sudanorg').close();
+    window.open('https://linktr.ee/PappyOrion', 'pappyorion').close();
+    window.open('https://www.sydney.edu.au/news-opinion/news/2023/10/25/uncharted-territory-climate-scientists-sound-alarm-over-earth-vital-signs.html', 'climate').close();
+    window.open('https://newrepublic.com/article/178678/supreme-court-criminalize-homeless-case', 'unhoused').close();
+    window.open('https://images.squarespace-cdn.com/content/v1/5ac2e04c4611a0b878a29437/1612344272870-BXUJITU7ESU5AG4Q7TZ3/Screen%2BShot%2B2021-02-03%2Bat%2B1.23.00%2BAM.jpg', 'burgepic').close();
+    window.open('https://www.npr.org/transcripts/886945461', 'burge', 'width=400,height=400,left=600,top=300x').close();
+    window.open('https://www.npr.org/sections/goatsandsoda/2023/02/01/1152893248/red-cobalt-congo-drc-mining-siddharth-kara', 'cobaltrednpr').close();
+    window.open('https://pbs.twimg.com/media/GA_smtDXwAAtOde.jpg:large', 'alareer').close();
+    window.open('https://www.mediamatic.net/image/2020/8/21/de_que_otra_cosa_podriamos_hablar_3web.jpg%28mediaclass-portrait-large.88f7636fba7a77601a1d39407ef2e0585a37698d%29.jpg', 'flag').close();
+    window.open('https://www.mediamatic.net/en/page/378691/traces-of-death', 'traces').close();
+    window.open('https://i.redd.it/what-is-your-favourite-manga-panel-v0-gznkbh5vtnxc1.jpg?width=669&format=pjpg&auto=webp&s=ed3723d3ac0677db71828ad328701c825fac74ee', 'robin').close();
+    window.open('https://www.besttherapists.com/blog/am-i-bipolar-or-depressed-quiz', 'bipolarquiz').close();
+    window.open('https://reverehealth.com/live-better/asthma-attacks-what-to-do-when-you-dont-have-your-inhaler/', 'inhaler').close();
+    window.open('https://www.tiktok.com/@pappyorion/video/7365025092876717344?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530', 'congott').close();
+    window.open('https://media.licdn.com/dms/image/C4E22AQEJMfwDXRjRHw/feedshare-shrink_2048_1536/0/1652736379352?e=2147483647&v=beta&t=uZmDRzdGOma8F5aiCRdTGwg9Un3Rf9O8X1LZuteo-RM', 'tigrayinfo').close();
+    window.open('https://p4hglobal.org/give', 'haitiorg').close();
+  }
+
+  // Function to turn off  audio
+  function turnOffAudio() {
+    document.querySelectorAll('audio').forEach(audio => {
+      audio.pause();
+      audio.currentTime = 0; // Reset the playback position
+    });
+  }
+
+  // Function to toggle blackout effect
+  function toggleBlackout(show) {
+    blackoutDiv.style.display = show ? 'block' : 'none';
+  }
+
+  // Detect hover over a specific position (e.g., an element with the ID 'hover-target')
+  const hoverTarget = document.getElementById('hover-target');
+  hoverTarget.addEventListener('mouseenter', () => {
+    closePopups();
+    turnOffAudio();
+    toggleBlackout(true);
+  });
+  hoverTarget.addEventListener('mouseleave', () => {
+    toggleBlackout(false);
+  });
 });
