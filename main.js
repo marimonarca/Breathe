16b8f6c025297720fd0b32b5fcf02db5b306fe85
maria <mariafernanda@uchicago.edu>
2024-05-14 20:21:26 -0500

    added popups

--- main.js
+++ main.js
@@ -1,7 +1,234 @@
-
+//music videos//
+// Mexico HMR video //
 function openPopup () {
  let html = 'https://www.youtube.com/embed/nQiS0UasJW4?si=nrdrBVF8uwY11FTm'
  let w = '_blank'
  let options = 'width=300,height=300,left=500,top=0x'
+ window.open(html, w, options)
+}
+// shosty symphony//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=-Us8ElJ3yUY&t=315s'
+ let w = '_blank'
+ let options = 'width=300,height=300,right=200,top=500x'
+ window.open(html, w, options)
+}
+//santana//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=JO6_Ttzila8'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=400,top=400x'
+ window.open(html, w, options)
+}
+//cielito lindo//
+function openPopup () {
+ let html = 'https://youtu.be/yjJDv1IeF8I?si=nZOObewMZZ4Kz3-L'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=200,top=500x'
+ window.open(html, w, options)
+}
+//sza good days//
+function openPopup () {
+ let html = 'https://youtu.be/2p3zZoraK9g?si=6-Fljw_yORfWMEMU'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=100,top=0x'
+ window.open(html, w, options)
+}
+//kendrick scream//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=W3Ww9wigdso'
+ let w = '_blank'
+ let options = 'width=300,height=300,right=100,bottom=0x'
+ window.open(html, w, options)
+}
+//movie scenes//
+//spiderman//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=-euUGPQZoHw'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=300,top=200x'
+ window.open(html, w, options)
+}
+//roma//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=Wy61WCo6jqI'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=200,top=250x'
+ window.open(html, w, options)
+}
+//eeaao//
+function openPopup () {
+ let html = 'https://youtu.be/vT0Y3k32QRE?si=-wsmldOVeycDCBPT'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=500,top=400x'
+ window.open(html, w, options)
+}
+//killbill//
+function openPopup () {
+ let html = 'https://youtu.be/ecimuK20dSA?si=UVwpzbYWpIipW6Et'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=800,top=800x'
+ window.open(html, w, options)
+}
+
+//vids//
+//sudan hrw//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=GWriwq7scRA'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=800,top=300x'
+ window.open(html, w, options)
+}
+//monkey study//
+function openPopup () {
+ let html = 'https://www.youtube.com/watch?v=OrNBEhzjg8I&t=142s'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=800,top=700x'
+ window.open(html, w, options)
+}
+
+//tiktoks//
+//congo//
+function openPopup () {
+ let html = 'https://www.tiktok.com/@pappyorion/video/7360692095633411361?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=900,top=400x'
+ window.open(html, w, options)
+}
+//congo//
+function openPopup () {
+ let html = 'https://www.tiktok.com/@pappyorion/video/7365025092876717344?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=7500,top=850x'
+ window.open(html, w, options)
+}
+//haiti//
+function openPopup () {
+ let html = 'https://www.tiktok.com/@zaylewismusic/video/7364232810363637023?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=300,top=700x'
+ window.open(html, w, options)
+}
+//pal//
+function openPopup () {
+ let html = 'https://www.tiktok.com/@ajohn095/video/7366317588797852971?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=200,top=800x'
+ window.open(html, w, options)
+}
+
+//webpages//
+//recetas//
+function openPopup () {
+ let html = 'https://www.univision.com/delicioso/30-recetas-altas-en-proteinas-y-bajas-en-carbohidratos-que-amaras-conocer'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=900,top=200x'
+ window.open(html, w, options)
+}
+//maroon//
+function openPopup () {
+ let html = 'https://chicagomaroon.com/25853/news/rallies-sit-ins-padlocks-history-trauma-activism/'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=750,top=650x'
+ window.open(html, w, options)
+}
+
+//cbt techniques//
+function openPopup () {
+ let html = 'https://positivepsychology.com/cbt-cognitive-behavioral-therapy-techniques-worksheets/'
+ let w = '_blank'
+ let options = 'width=400,height=400,left=300,top=300x'
+ window.open(html, w, options)
+}
+//cobalt red npr//
+function openPopup () {
+ let html = 'https://www.npr.org/sections/goatsandsoda/2023/02/01/1152893248/red-cobalt-congo-drc-mining-siddharth-kara'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=700,top=200x'
+ window.open(html, w, options)
+}
+//cabalt red pic//
+function openPopup () {
+ let html = 'https://m.media-amazon.com/images/I/918ckvZTCsL._AC_UF1000,1000_QL80_.jpg'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=700,top=300x'
+ window.open(html, w, options)
+}
+//sudan pic//
+function openPopup () {
+ let html = 'https://www.hrw.org/modal/34478'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=200,top=800x'
+ window.open(html, w, options)
+}
+//tigray hrw//
+function openPopup () {
+ let html = 'https://www.hrw.org/news/2023/06/01/ethiopia-ethnic-cleansing-persists-under-tigray-truce'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=400,top=900x'
+ window.open(html, w, options)
+}
+//nestle//
+function openPopup () {
+ let html = 'https://www.theguardian.com/global/2018/oct/04/ontario-six-nations-nestle-running-water'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=500,top=200x'
+ window.open(html, w, options)
+}
+//burge//
+function openPopup () {
+ let html = 'https://www.npr.org/transcripts/886945461'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=600,top=300x'
+ window.open(html, w, options)
+}
+//burge pic//
+function openPopup () {
+ let html = 'https://images.squarespace-cdn.com/content/v1/5ac2e04c4611a0b878a29437/1612344272870-BXUJITU7ESU5AG4Q7TZ3/Screen%2BShot%2B2021-02-03%2Bat%2B1.23.00%2BAM.jpg'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=300,top=800x'
+ window.open(html, w, options)
+}
+//unhoused//
+function openPopup () {
+ let html = 'https://newrepublic.com/article/178678/supreme-court-criminalize-homeless-case'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=600,top=600x'
+ window.open(html, w, options)
+}
+//climate//
+function openPopup () {
+ let html = 'https://www.sydney.edu.au/news-opinion/news/2023/10/25/uncharted-territory-climate-scientists-sound-alarm-over-earth-vital-signs.html'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=200,top=800x'
+ window.open(html, w, options)
+}
+//fundraiser//
+//congo fundraiser//
+function openPopup () {
+ let html = 'https://www.focuscongo.com/en/spende/'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=600,top=400x'
+ window.open(html, w, options)
+}
+//pappyorion//
+function openPopup () {
+ let html = 'https://linktr.ee/PappyOrion'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=250,top=250x'
+ window.open(html, w, options)
+}
+//sudan org//
+function openPopup () {
+ let html = 'https://sapa-usa.org/Hope-for-Sudan/?fbclid=PAAaZMr4VAjKinWXZXucTTx82fQxYafFbbIHnVsdPVT99U6I3vXyohAJvi-Z4_aem_AURgfG7WDmkUyTlraghddZvjJ5hZm0dwAlvMNLoHM5s8xB7H4RjwC9huUkRpF0ZTG0I'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=250,top=250x'
+ window.open(html, w, options)
+}
+//tigray//
+function openPopup () {
+ let html = 'https://linktr.ee/Tdrfund.org'
+ let w = '_blank'
+ let options = 'width=300,height=300,left=250,top=250x'
  window.open(html, w, options)
 }
