fbab4db6b0a366d5d0335e97e9c69f2d775ffb50
maria <mariafernanda@uchicago.edu>
2024-05-14 22:59:46 -0500

    bigger popups

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 //it works!//
 function openMultiplePopups() {
-  window.open('https://www.youtube.com/embed/nQiS0UasJW4?si=nrdrBVF8uwY11FTm', 'mexicohmr', 'width=300,height=300,left=500,top=0x');
-  window.open('https://www.youtube.com/watch?v=-euUGPQZoHw', 'spiderman', 'width=300,height=300,left=300,top=200x');
-  window.open('https://www.tiktok.com/@pappyorion/video/7360692095633411361?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530', 'congo', 'width=300,height=300,left=900,top=400x');
-  window.open('https://www.univision.com/delicioso/30-recetas-altas-en-proteinas-y-bajas-en-carbohidratos-que-amaras-conocer', 'recetas', 'width=300,height=300,left=900,top=200x');
-  window.open('https://linktr.ee/Tdrfund.org', 'tigraylinks', 'width=300,height=300,left=250,top=250x');
-  window.open('https://youtu.be/2p3zZoraK9g?si=6-Fljw_yORfWMEMU', 'sza', 'width=300,height=300,left=100,top=0x');
-  window.open('https://youtu.be/vT0Y3k32QRE?si=-wsmldOVeycDCBPT', 'eeaao', 'width=300,height=300,left=500,top=400x');
-  window.open('https://www.youtube.com/watch?v=GWriwq7scRA', 'sudanhrw', 'width=300,height=300,left=800,top=300x');
-  window.open('https://chicagomaroon.com/25853/news/rallies-sit-ins-padlocks-history-trauma-activism/', 'maroon', 'width=300,height=300,left=750,top=650x');
+  window.open('https://www.youtube.com/embed/nQiS0UasJW4?si=nrdrBVF8uwY11FTm', 'mexicohmr', 'width=400,height=400,left=500,top=0x');
+  window.open('https://www.youtube.com/watch?v=-euUGPQZoHw', 'spiderman', 'width=400,height=400,left=300,top=200x');
+  window.open('https://www.tiktok.com/@pappyorion/video/7360692095633411361?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530', 'congo', 'width=400,height=400,left=900,top=400x');
+  window.open('https://www.univision.com/delicioso/30-recetas-altas-en-proteinas-y-bajas-en-carbohidratos-que-amaras-conocer', 'recetas', 'width=400,height=400,left=900,top=200x');
+  window.open('https://linktr.ee/Tdrfund.org', 'tigraylinks', 'width=400,height=400,left=950,top=950x');
+  window.open('https://youtu.be/2p3zZoraK9g?si=6-Fljw_yORfWMEMU', 'sza', 'width=400,height=400,left=100,top=0x');
+  window.open('https://youtu.be/vT0Y3k32QRE?si=-wsmldOVeycDCBPT', 'eeaao', 'width=400,height=400,left=500,top=400x');
+  window.open('https://www.youtube.com/watch?v=GWriwq7scRA', 'sudanhrw', 'width=400,height=400,left=800,top=300x');
+  window.open('https://chicagomaroon.com/25853/news/rallies-sit-ins-padlocks-history-trauma-activism/', 'maroon', 'width=400,height=400,left=750,top=650x');
   window.open('https://positivepsychology.com/cbt-cognitive-behavioral-therapy-techniques-worksheets/', 'cbt', 'width=400,height=400,left=300,top=300x');
-  window.open('https://m.media-amazon.com/images/I/918ckvZTCsL._AC_UF1000,1000_QL80_.jpg', 'cobaltred', 'width=300,height=300,left=700,top=300x');
-  window.open('https://www.hrw.org/modal/34478', 'sudanpic', 'width=300,height=300,left=200,top=800x');
-  window.open('https://www.hrw.org/news/2023/06/01/ethiopia-ethnic-cleansing-persists-under-tigray-truce', 'tigray', 'width=300,height=300,left=400,top=900x');
-  window.open('https://www.theguardian.com/global/2018/oct/04/ontario-six-nations-nestle-running-water', 'nestle', 'width=300,height=300,left=500,top=200x');
-  window.open('https://sapa-usa.org/Hope-for-Sudan/?fbclid=PAAaZMr4VAjKinWXZXucTTx82fQxYafFbbIHnVsdPVT99U6I3vXyohAJvi-Z4_aem_AURgfG7WDmkUyTlraghddZvjJ5hZm0dwAlvMNLoHM5s8xB7H4RjwC9huUkRpF0ZTG0I', 'sudanorg', 'width=300,height=300,left=250,top=250x');
-  window.open('https://linktr.ee/PappyOrion', 'pappyorion', 'width=300,height=300,left=250,top=250x');
-  window.open('https://www.sydney.edu.au/news-opinion/news/2023/10/25/uncharted-territory-climate-scientists-sound-alarm-over-earth-vital-signs.html', 'climate', 'width=300,height=300,left=200,top=800x');
-  window.open('https://newrepublic.com/article/178678/supreme-court-criminalize-homeless-case', 'unhoused', 'width=300,height=300,left=600,top=600x');
+  window.open('https://m.media-amazon.com/images/I/918ckvZTCsL._AC_UF1000,1000_QL80_.jpg', 'cobaltred', 'width=400,height=400,left=700,top=300x');
+  window.open('https://www.hrw.org/modal/34478', 'sudanpic', 'width=400,height=400,left=200,top=800x');
+  window.open('https://www.hrw.org/news/2023/06/01/ethiopia-ethnic-cleansing-persists-under-tigray-truce', 'tigray', 'width=400,height=400,left=400,top=900x');
+  window.open('https://www.theguardian.com/global/2018/oct/04/ontario-six-nations-nestle-running-water', 'nestle', 'width=400,height=400,left=500,top=200x');
+  window.open('https://sapa-usa.org/Hope-for-Sudan/?fbclid=PAAaZMr4VAjKinWXZXucTTx82fQxYafFbbIHnVsdPVT99U6I3vXyohAJvi-Z4_aem_AURgfG7WDmkUyTlraghddZvjJ5hZm0dwAlvMNLoHM5s8xB7H4RjwC9huUkRpF0ZTG0I', 'sudanorg', 'width=300,height=300,left=950,top=50x');
+  window.open('https://linktr.ee/PappyOrion', 'pappyorion', 'width=400,height=400,left=100,top=250x');
+  window.open('https://www.sydney.edu.au/news-opinion/news/2023/10/25/uncharted-territory-climate-scientists-sound-alarm-over-earth-vital-signs.html', 'climate', 'width=400,height=400,left=200,top=800x');
+  window.open('https://newrepublic.com/article/178678/supreme-court-criminalize-homeless-case', 'unhoused', 'width=400,height=400,left=600,top=600x');
   window.open('https://images.squarespace-cdn.com/content/v1/5ac2e04c4611a0b878a29437/1612344272870-BXUJITU7ESU5AG4Q7TZ3/Screen%2BShot%2B2021-02-03%2Bat%2B1.23.00%2BAM.jpg', 'burgepic', 'width=300,height=300,left=300,top=800x');
-  window.open('https://www.npr.org/transcripts/886945461', 'burge', 'width=300,height=300,left=600,top=300x');
-  window.open('https://www.npr.org/sections/goatsandsoda/2023/02/01/1152893248/red-cobalt-congo-drc-mining-siddharth-kara', 'cobaltrednpr', 'width=300,height=300,left=700,top=200x');
-  window.open('https://pbs.twimg.com/media/GA_smtDXwAAtOde.jpg:large', 'alareer', 'width=300,height=300,left=800,top=500x');
+  window.open('https://www.npr.org/transcripts/886945461', 'burge', 'width=400,height=400,left=600,top=300x');
+  window.open('https://www.npr.org/sections/goatsandsoda/2023/02/01/1152893248/red-cobalt-congo-drc-mining-siddharth-kara', 'cobaltrednpr', 'width=400,height=400,left=700,top=200x');
+  window.open('https://pbs.twimg.com/media/GA_smtDXwAAtOde.jpg:large', 'alareer', 'width=400,height=400,left=800,top=500x');
   window.open('https://www.mediamatic.net/image/2020/8/21/de_que_otra_cosa_podriamos_hablar_3web.jpg%28mediaclass-portrait-large.88f7636fba7a77601a1d39407ef2e0585a37698d%29.jpg', 'flag', 'width=300,height=300,left=900,top=400x');
-  window.open('https://www.mediamatic.net/en/page/378691/traces-of-death', 'traces', 'width=300,height=300,left=300,top=200x');
-  window.open('https://i.redd.it/what-is-your-favourite-manga-panel-v0-gznkbh5vtnxc1.jpg?width=669&format=pjpg&auto=webp&s=ed3723d3ac0677db71828ad328701c825fac74ee', 'robin', 'width=300,height=300,left=450,top=650x');
+  window.open('https://www.mediamatic.net/en/page/378691/traces-of-death', 'traces', 'width=400,height=400,left=300,top=200x');
+  window.open('https://i.redd.it/what-is-your-favourite-manga-panel-v0-gznkbh5vtnxc1.jpg?width=669&format=pjpg&auto=webp&s=ed3723d3ac0677db71828ad328701c825fac74ee', 'robin', 'width=400,height=400,left=450,top=650x');
   window.open('https://www.besttherapists.com/blog/am-i-bipolar-or-depressed-quiz', 'bipolarquiz', 'width=300,height=300,left=600,top=850x');
+  window.open('https://reverehealth.com/live-better/asthma-attacks-what-to-do-when-you-dont-have-your-inhaler/', 'inhaler', 'width=400,height=400,left=50,top=950x')
+  window.open('https://www.tiktok.com/@pappyorion/video/7365025092876717344?is_from_webapp=1&sender_device=pc&web_id=7119675791289599530', 'congott', 'width=400,height=400,left=950,top=50x')
+  window.open('https://media.licdn.com/dms/image/C4E22AQEJMfwDXRjRHw/feedshare-shrink_2048_1536/0/1652736379352?e=2147483647&v=beta&t=uZmDRzdGOma8F5aiCRdTGwg9Un3Rf9O8X1LZuteo-RM', 'tigrayinfo', 'width=400,height=400,right=0,top=500x')
+  
 }
